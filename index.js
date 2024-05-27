4be9fb4e7d2cb6c446b3a538e6766c8d81a5ee80
QuangAnhTo <quangato072@gmail.com>
2024-05-27 21:59:17 +0700

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import productModel from "./models/products.js";
+
+await mongoose
+  .connect(
+    "mongodb+srv://savvyreactor:nhom2mindx@project-ss3.enwnglq.mongodb.net/project_ss3?retryWrites=true&w=majority&appName=project-ss3"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    // origin: "http://localhost:5173", // Replace with your React app's origin
+  })
+);
+
+app.get("/products", async (req, res) => {
+  const products = await productModel.find({});
+  res.send(products);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
