67461f5bb91ccd356a26ffb99aac7ac039073799
QuangAnhTo <quangato072@gmail.com>
2024-05-31 23:30:16 +0700

    feat: Add authentication middleware

---
+++ middlewares/authenticate.js
@@ -0,0 +1,18 @@
+import jwt, { decode } from "jsonwebtoken";
+
+const authenticate = (req, res, next) => {
+  const token = req.header("Authorization").split(" ")[1];
+  if (!token) {
+    return res.status(401).send("Access Denied");
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid Token");
+  }
+};
+
+export default authenticate;
