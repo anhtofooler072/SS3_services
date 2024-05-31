1f280d0e5d00e39d68e86c2d300edf1b2f7f3686
QuangAnhTo <quangato072@gmail.com>
2024-05-31 23:26:01 +0700

    feat: Add user credentials controller and routes

---
+++ controllers/credentials/userCred.js
@@ -0,0 +1,44 @@
+import userModel from "../../models/users";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userCredController = {
+  register: async (req, res) => {
+    try {
+      const { username, email, password, phone_number } = req.body;
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = new userModel({
+        username,
+        email,
+        password: hashedPassword,
+        phone_number,
+        salt,
+      });
+      await user.save();
+      res.send("User registered successfully");
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  },
+
+  tokenize: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const loginInfo = await userModel.findOne({ email });
+      if (!loginInfo) {
+        return res.status(400).send("Email không tồn tại");
+      }
+      const hashedPassword = await bcrypt.hash(password, loginInfo.salt);
+      if (hashedPassword !== loginInfo.password) {
+        return res.status(400).send("Mật khẩu không đúng");
+      }
+      const token = jwt.sign({ email }, process.env.TOKEN_SECRET);
+      res.status(200).send(token);
+    } catch (error) {
+      res.status;
+    }
+  },
+};
+
+export default userCredController;
---
+++ routes/users/userCred.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { userCredController } from "../../controllers/credentials/userCred.js";
+
+const credRouter = Router();
+
+credRouter.post("/register", userCredController.register);
+credRouter.post("/tokenize", userCredController.tokenize);
+
+export default credRouter;
