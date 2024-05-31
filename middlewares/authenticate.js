e40d1cdfcffa5db6e2db2a982fd1031b9265be9c
QuangAnhTo <quangato072@gmail.com>
2024-05-31 23:53:40 +0700

    chore: Refactor authentication middleware and userCred.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,18 +1,19 @@
-import jwt, { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-const authenticate = (req, res, next) => {
-  const token = req.header("Authorization").split(" ")[1];
-  if (!token) {
-    return res.status(401).send("Access Denied");
-  }
-
-  try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
-    next();
-  } catch (error) {
-    res.status(400).send("Invalid Token");
-  }
+const auth = {
+  verifyToken: (req, res, next) => {
+    try {
+      const token = req.header("Authorization").split(" ")[1];
+      if (!token) return res.status(401).send("Access Denied");
+      const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+      // req.user = verified;
+      console.log(verified); // testing purpose
+      res.send(verified);
+      // next();
+    } catch (error) {
+      res.status(400).send("Invalid Token");
+    }
+  },
 };
 
-export default authenticate;
+export default auth;
--- routes/users/userCred.js
+++ routes/users/userCred.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import userCredController from "../../controllers/credentials/userCred.js";
+import auth from "../../middlewares/authenticate.js";
+
 
 const credRouter = Router();
 
 credRouter.post("/register", userCredController.register);
 credRouter.post("/tokenize", userCredController.tokenize);
+credRouter.post("/authtest", auth.verifyToken);
 
 export default credRouter;
