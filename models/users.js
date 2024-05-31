bb086e4f3990f3299b1c0bdc593077a298b6760c
QuangAnhTo <quangato072@gmail.com>
2024-05-31 23:05:16 +0700

    chore: Add phone_number field to user model

--- models/users.js
+++ models/users.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  phone_number: {
+    type: String,
+    required: true,
+  },
   salt: {
     type: String,
     required: true,
