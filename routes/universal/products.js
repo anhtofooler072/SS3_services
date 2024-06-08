cf759396abe8642307162815013d9b2593c7e1db
QuangAnhTo <quangato072@gmail.com>
2024-06-08 21:57:14 +0700

    feat: add uri for checkout

--- routes/universal/products.js
+++ routes/universal/products.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import productsController from "../../controllers/products/products.js";
+import orderingController from "../../controllers/ordering/ordering.js";
 
 const universalRouter = Router();
 
 universalRouter.get("/fetchproducts", productsController.getProducts);
 universalRouter.get("/fetchproducts/:id", productsController.getProductById);
+universalRouter.post("/ordering", orderingController.checkout);
 
 export default universalRouter;
